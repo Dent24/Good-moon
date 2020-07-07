399aa9eab2ebaa4644d2e4000b79a04b7b185daf
Dent24 <cljhs90607@gmail.com>
2020-07-07 16:31:58 +0800

    js控制導覽列

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        var windowTop = $(window).scrollTop();
+        var offset = $(about).offset();
+        var top = offset.top;
+        top = top - 300;
+
+        if(windowTop > top){
+            $("#nav").show(400);
+        } else{
+            $("#nav").hide();
+        }
+    })
+})
