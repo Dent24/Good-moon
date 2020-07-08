b7da16a0dd4cf99afdcec296a54d097f55dad24a
Dent24 <cljhs90607@gmail.com>
2020-07-08 11:15:40 +0800

    news 修改

--- mainNews.js
+++ mainNews.js
@@ -1,7 +1,23 @@
 $('#slide').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
-    prevArrow: $('.prev'),
-    nextArrow: $('.next'),
+    prevArrow: $('#prev'),
+    nextArrow: $('#next'),
     infinite: false,
+
+    dots: true,
+    customPaging : function(slider, i) {
+    var thumb = $(slider.$slides[i]).data();
+    return '<a class="dot">'+(i+1)+'</a>';
+            },
+    responsive: [{ 
+        breakpoint: 500,
+        settings: {
+            dots: false,
+            arrows: false,
+            infinite: false,
+            slidesToShow: 2,
+            slidesToScroll: 2
+        } 
+    }]
 });
