001e8a9bb4cc5c1af44211cdc16dcacf5c1fce65
Dent24 <65475471+Dent24@users.noreply.github.com>
2020-07-12 14:09:25 +0800

    回到頂端動畫增加

--- main.js
+++ main.js
@@ -10,8 +10,31 @@ $(document).ready(function(){
             $("#nav").show(400);
             $("#right").show();
         } else{
-            $("#nav").hide();
+            $("#nav").hide(400);
             $("#right").hide();
         }
     })
+
+
+    var elements = document.getElementsByTagName("*");
+
+    for (let index = 0; index < elements.length; index++) {
+        const element = elements[index];
+
+        let t = element.getAttribute("data-gte-target");
+        let o = element.getAttribute("data-gte-offset");
+
+        if (t) {
+
+            element.onclick = () => {
+
+                let tar = document.getElementById(t);
+
+                window.scrollTo({
+                    top: tar.offsetTop - parseInt(o),
+                    behavior: "smooth",
+                });
+            }
+        }
+    }
 })
