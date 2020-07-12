b0d4e80cfc966be1af80bd406c5b5bf90949e770
Dent24 <65475471+Dent24@users.noreply.github.com>
2020-07-12 14:24:48 +0800

    修改動畫

--- main.js
+++ main.js
@@ -14,27 +14,4 @@ $(document).ready(function(){
             $("#right").hide();
         }
     })
-
-
-    var elements = document.getElementsByTagName("*");
-
-    for (let index = 0; index < elements.length; index++) {
-        const element = elements[index];
-
-        let t = element.getAttribute("data-gte-target");
-        let o = element.getAttribute("data-gte-offset");
-
-        if (t) {
-
-            element.onclick = () => {
-
-                let tar = document.getElementById(t);
-
-                window.scrollTo({
-                    top: tar.offsetTop - parseInt(o),
-                    behavior: "smooth",
-                });
-            }
-        }
-    }
 })
